e62747fc72bbf7e50aeab31b680440ffa211d928
Jacek Salamon <ajcek84@gmail.com>
2014-08-03 10:05:37 +0200

    Parser do konturu melodycznego

---
+++ Manufaktura.Controls/Parser/Digest/MelodicContourDigestParser.cs
@@ -0,0 +1,34 @@
+﻿using Manufaktura.Controls.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Manufaktura.Controls.Parser.Digest
+{
+    class MelodicContourDigestParser : ScoreParser<int[]>
+    {
+        public override Model.Score Parse(int[] source)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int[] ParseBack(Model.Score score)
+        {
+            List<int> intervals = new List<int>();
+            foreach (Staff staff in score.Staves)
+            {
+                Note lastNote = null;
+                foreach (Note note in staff.Elements.OfType<Note>())
+                {
+                    if (lastNote != null)
+                    {
+                        intervals.Add(note.MidiPitch - lastNote.MidiPitch);
+                    }
+                    lastNote = note;
+                }
+            }
+            return intervals.ToArray();
+        }
+    }
+}
