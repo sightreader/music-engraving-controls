12b43d9ae72eff32cd76c01db014dd3602346cfb
Jacek Salamon <ajcek84@gmail.com>
2014-09-18 20:01:38 +0200

    HighestNoteInChordPeekStrategy

--- Manufaktura.Controls/Model/PeekStrategies/HighestNoteInChordPeekStrategy.cs
+++ Manufaktura.Controls/Model/PeekStrategies/HighestNoteInChordPeekStrategy.cs
@@ -9,11 +9,21 @@ namespace Manufaktura.Controls.Model.PeekStrategies
     {
         public HighestNoteInChordPeekStrategy(Staff staff) : base(staff)
         {
+            if (typeof(TSymbol) != typeof(Note)) throw new Exception("This strategy can be only used for notes.");
         }
 
         public override TSymbol Peek(MusicalSymbol relativeTo)
         {
-            throw new NotImplementedException();
+            Note lowestNote = relativeTo as Note;
+            List<Note> chordElements = new List<Note>() { lowestNote };
+            for (int i = Staff.Elements.IndexOf(lowestNote) + 1; i < Staff.Elements.Count(); i++)
+            {
+                Note currentNote = Staff.Elements[i] as Note;
+                if (currentNote == null) continue;
+                if (currentNote.IsChordElement) chordElements.Add(currentNote);
+                else break;
+            }
+            return chordElements.First(n => n.MidiPitch == chordElements.Max(n1 => n1.MidiPitch)) as TSymbol;
         }
     }
 }
