573c5751d00594c798f7f917667f52a3b70442ba
Jacek Salamon <ajcek84@gmail.com>
2014-10-07 23:13:49 +0200

    Orm

--- Manufaktura.Orm.MySql/MySqlCommandBuilder.cs
+++ Manufaktura.Orm.MySql/MySqlCommandBuilder.cs
@@ -33,12 +33,76 @@ namespace Manufaktura.Orm.Builder
 
         public override DbCommand GetInsertCommand(Entity entity)
         {
-            throw new NotImplementedException();
+            MappingAttribute typeAttribute = entity.GetType().GetCustomAttributes(typeof(MappingAttribute), true).FirstOrDefault() as MappingAttribute;
+            if (typeAttribute == null) throw new Exception(string.Format("Nazwa tabeli nie została określona dla typu {0}.", entity.GetType().Name));
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("INSERT INTO ");
+            sb.Append(typeAttribute.Name);
+            sb.Append(" (");
+            bool first = true;
+            foreach (var property in entity.GetType().GetProperties())
+            {
+                if (property.GetValue(entity) == null) continue;
+                MappingAttribute attribute = property.GetCustomAttributes(typeof(MappingAttribute), true).FirstOrDefault() as MappingAttribute;
+                if (attribute == null) continue;
+                if (attribute.IsSpecialColumn) continue;
+                if (!first) sb.Append(", ");
+                sb.Append(string.Format("{0}", attribute.Name));
+                first = false;
+            }
+            sb.Append(") VALUES (");
+            first = true;
+            foreach (var property in entity.GetType().GetProperties())
+            {
+                if (property.GetValue(entity) == null) continue;
+                MappingAttribute attribute = property.GetCustomAttributes(typeof(MappingAttribute), true).FirstOrDefault() as MappingAttribute;
+                if (attribute == null) continue;
+                if (attribute.IsSpecialColumn) continue;
+                if (!first) sb.Append(", ");
+                sb.Append(string.Format("{0}", FormatValue(property.GetValue(entity))));
+                first = false;
+            }
+            sb.Append(")");
+
+            DbCommand command = Connection.CreateCommand();
+            command.CommandText = sb.ToString();
+            return command;
         }
 
         public override DbCommand GetUpdateCommand(Entity entity)
         {
-            throw new NotImplementedException();
+            MappingAttribute typeAttribute = entity.GetType().GetCustomAttributes(typeof(MappingAttribute), true).FirstOrDefault() as MappingAttribute;
+            if (typeAttribute == null) throw new Exception(string.Format("Nazwa tabeli nie została określona dla typu {0}.", entity.GetType().Name));
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("UPDATE ");
+            sb.Append(typeAttribute.Name);
+            sb.Append(" SET ");
+            bool first = true;
+            PropertyInfo idProperty = null;
+            foreach (var property in entity.GetType().GetProperties())
+            {
+                if (property.GetValue(entity) == null) continue;
+                MappingAttribute attribute = property.GetCustomAttributes(typeof(MappingAttribute), true).FirstOrDefault() as MappingAttribute;
+                if (attribute == null) continue;
+                if (attribute.IsSpecialColumn) continue;
+                if (attribute.IsPrimaryKey)
+                {
+                    idProperty = property;
+                    continue;
+                }
+                if (!first) sb.Append(", ");
+                sb.Append(string.Format("{0} = {1}", attribute.Name, FormatValue(property.GetValue(entity))));
+                first = false;
+            }
+            if (idProperty == null) throw new Exception("Brak kolumny id.");
+            MappingAttribute idAttribute = idProperty.GetCustomAttributes(typeof(MappingAttribute), true).FirstOrDefault() as MappingAttribute;
+            sb.Append(string.Format(" WHERE {0}={1};", idAttribute.Name, idProperty.GetValue(entity)));
+
+            DbCommand command = Connection.CreateCommand();
+            command.CommandText = sb.ToString();
+            return command;
         }
 
         public override DbCommand GetDeleteCommand(object id)
@@ -89,13 +153,21 @@ namespace Manufaktura.Orm.Builder
                 }
             }
 
-            if (Limit > 0)  //TODO: Wprowadzić obsługę dialektów SQL
+            if (Limit > 0)
             {
                 sb.Append(" LIMIT ");
                 if (Offset > 0) sb.Append(string.Format(" {0},", Offset));
                 sb.Append(string.Format(" {0}", Limit));
             }
             return sb.ToString();
+        }
+
+        [Obsolete("Używać parametrów")]
+        private string FormatValue(object value)
+        {
+            if (value is string) return string.Format("'{0}'", value == null ? value : ((string)value).Replace("'", ""));
+            if (value is Guid) return string.Format("'{0}'", value);
+            else return value.ToString();
         }
     }
 }
---
+++ Manufaktura.Orm/Repositories/DbRepository.cs
@@ -0,0 +1,66 @@
+﻿using Manufaktura.Orm.SortModes;
+using Manufaktura.Orm.SpecialColumns;
+using Manufaktura.Orm.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Web;
+using Manufaktura.Orm.Builder;
+
+namespace Manufaktura.Orm
+{
+    public abstract class DbRepository<TConnection, TAdapter> : IDisposable
+        where TConnection : DbConnection, new()
+        where TAdapter : DbDataAdapter, new()
+    {
+        public DbConnection Connection { get; protected set; }
+        protected abstract DbConnection CreateConnection(string connectionString);
+        public CommandBuilder DefaultCommandBuilder { get; protected set; }
+
+        protected DbRepository(CommandBuilder defaultCommandBuilder, string connectionString)
+        {
+            DefaultCommandBuilder = defaultCommandBuilder;
+            Connection = CreateConnection(connectionString);
+        }
+
+        public List<TEntity> LoadAll<TEntity>() where TEntity : Entity, new()
+        {
+            return Load<TEntity>(DefaultCommandBuilder);
+        }
+
+        public List<TEntity> Load<TEntity>(CommandBuilder builder) where TEntity : Entity, new()
+        {
+            DbCommand command = Connection.CreateCommand();
+            Connection.Open();
+            command.Connection = Connection;
+            TAdapter adapter = new TAdapter();
+            adapter.SelectCommand = builder.GetSelectCommand<TEntity>();
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            List<TEntity> results = new List<TEntity>();
+            foreach (DataRow row in table.Rows)
+            {
+                results.Add(Entity.FromRow<TEntity>(row));
+            }
+            return results;
+        }
+
+
+        public Entity Save(Entity entity)
+        {
+            DbCommand command = Connection.CreateCommand();
+            Connection.Open();
+            command = entity.IsNew ? DefaultCommandBuilder.GetInsertCommand(entity) : DefaultCommandBuilder.GetUpdateCommand(entity);
+            command.ExecuteNonQuery();
+            entity.IsNew = false;
+            return entity;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+
+}
