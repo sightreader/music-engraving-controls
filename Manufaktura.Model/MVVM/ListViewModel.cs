b228b7731cbd3f644de5399bef64ab94bf4fb802
Jacek Salamon <ajcek84@gmail.com>
2014-12-06 12:42:53 +0100

    Refaktor

---
+++ Manufaktura.Model/MVVM/ListViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Manufaktura.Model.MVVM
+{
+    public abstract class ListViewModel<TCollection, TElement> : ViewModel where TCollection : IEnumerable<TElement>, new()
+    {
+        private TCollection _elements;
+        public TCollection Elements
+        {
+            get { return _elements; }
+            set { _elements = value; OnPropertyChanged(() => Elements); }
+        }
+
+        private ListViewModelStates _state;
+        public ListViewModelStates State
+        {
+            get { return _state; }
+            set { _state = value; OnPropertyChanged(() => State); }
+        }
+
+        protected ListViewModel()
+        {
+            Elements = new TCollection();
+            State = ListViewModelStates.Idle;
+        }
+    }
+}
