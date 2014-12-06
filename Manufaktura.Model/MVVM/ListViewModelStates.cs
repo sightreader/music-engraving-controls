a4d6f808f4375273d32cb6f7a1a05a8ca2a1e129
Jacek Salamon <ajcek84@gmail.com>
2014-12-06 12:32:21 +0100

    Refaktor

---
+++ Manufaktura.Model/MVVM/ListViewModelStates.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Manufaktura.Model.MVVM
+{
+    public enum ListViewModelStates
+    {
+        Idle,
+        Progress,
+        NoResults,
+        Error,
+        Results
+    }
+}
