1b60aaa518ac616436e0ceb5ba037dff2c3d065e
Jacek Salamon <ajcek84@gmail.com>
2014-12-08 20:02:25 +0100

    Przeniesienie ViewModelCommand do bibliotek

---
+++ Manufaktura.Controls.Silverlight/Commands/ViewModelCommand.cs
@@ -0,0 +1,35 @@
+﻿using Manufaktura.Controls.Silverlight.Commands;
+using Manufaktura.Model.MVVM;
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Manufaktura.Controls.Silverlight.Commands
+{
+    public abstract class ViewModelCommand<TViewModel, TParameter> : Command<TParameter> where TViewModel : ViewModel
+    {
+        public TViewModel CurrentViewModel { get; protected set; }
+
+        protected ViewModelCommand(TViewModel currentViewModel)
+        {
+            CurrentViewModel = currentViewModel;
+        }
+    }
+
+    public abstract class ViewModelCommand<TViewModel> : Command where TViewModel : ViewModel
+    {
+        public TViewModel CurrentViewModel { get; protected set; }
+
+        protected ViewModelCommand(TViewModel currentViewModel)
+        {
+            CurrentViewModel = currentViewModel;
+        }
+    }
+}
