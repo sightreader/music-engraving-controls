648bc0ae378212927e0020f7b2cce6f115df309e
Jacek Salamon <ajcek84@gmail.com>
2015-02-22 22:09:16 +0100

    Teoria muzyki - rozwój modelu

--- Manufaktura.Music/MusicTheory/Model/Gamut.cs
+++ Manufaktura.Music/MusicTheory/Model/Gamut.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Manufaktura.Controls.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,32 @@ namespace Manufaktura.Music.MusicTheory.Model
     public abstract class Gamut
     {
         public abstract IEnumerable<int> Intervals { get; }
+
+        public IEnumerable<Note> BuildSteps(int startingMidiPitch, int startingStep, int numberOfSteps)
+        {
+            if (startingStep < 1) throw new ArgumentException("Starting step must be greater than 0.", "startingStep");
+            if (numberOfSteps < 1) throw new ArgumentException("Number of steps must be greater than 0.", "numberOfSteps");
+
+            var notes = new List<Note>();
+            var midiPitch = startingMidiPitch;
+            for (int step = startingStep; step < startingStep + numberOfSteps; step++)
+            {
+                notes.Add(Note.FromMidiPitch(midiPitch));
+                midiPitch = midiPitch + GetIntervalAfterStep(step);
+            }
+            return notes;
+        }
+
+        public int GetIntervalAfterStep(int step)
+        {
+            if (step < 1) throw new ArgumentException("Step must be greater than 0.", "step");
+            var index = step - 1;
+            var intervalsList = Intervals.ToList();
+            while (index >= intervalsList.Count)
+            {
+                index -= intervalsList.Count;
+            }
+            return intervalsList[index];
+        }
     }
 }
--- Manufaktura.Music/MusicTheory/Model/Scale.cs
+++ Manufaktura.Music/MusicTheory/Model/Scale.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Manufaktura.Controls.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,11 @@ namespace Manufaktura.Music.MusicTheory.Model
         protected Scale(int startingMidiPitch)
         {
             StartingMidiPitch = startingMidiPitch;
+        }
+
+        public IEnumerable<Note> BuildSteps(int startingStep, int numberOfSteps)
+        {
+            return Mode.Gamut.BuildSteps(StartingMidiPitch, startingStep, numberOfSteps);
         }
     }
 }
