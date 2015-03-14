7ad3cd2e664c62a6aa21b18865fa6dbc51003d81
Jacek Salamon <ajcek84@gmail.com>
2015-03-14 20:44:18 +0100

    Podejście do zapisywania MusicXml

---
+++ Manufaktura.Controls/Parser/MusicXml/Strategies/ClefWritingStrategy.cs
@@ -0,0 +1,23 @@
+﻿using Manufaktura.Controls.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Manufaktura.Controls.Parser.MusicXml.Strategies
+{
+    class ClefWritingStrategy : MusicXmlWritingStrategy<Clef>
+    {
+        public override string ElementName
+        {
+            get { return "clef"; }
+        }
+
+        protected override void WriteElementInner(Clef symbol, XElement element)
+        {
+            element.Add("sign", symbol.Pitch.StepName);
+            element.Add("line", symbol.Line);
+        }
+    }
+}
---
+++ Manufaktura.Controls/Parser/MusicXml/Strategies/MusicXmlWritingStrategyBase.cs
@@ -0,0 +1,43 @@
+﻿using Manufaktura.Controls.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Manufaktura.Controls.Parser.MusicXml.Strategies
+{
+    public abstract class MusicXmlWritingStrategyBase
+    {
+        private static IEnumerable<MusicXmlWritingStrategyBase> _strategies;
+
+        public abstract Type SymbolType { get; }
+        public abstract string ElementName { get; }
+
+        static MusicXmlWritingStrategyBase()
+        {
+            var strategyTypes = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsSubclassOf(typeof(MusicXmlWritingStrategyBase)) && !t.IsAbstract);
+            List<MusicXmlWritingStrategyBase> strategies = new List<MusicXmlWritingStrategyBase>();
+            foreach (Type type in strategyTypes)
+            {
+                strategies.Add(Activator.CreateInstance(type) as MusicXmlWritingStrategyBase);
+            }
+            _strategies = strategies.ToArray();
+        }
+
+        public void WriteElement(MusicalSymbol symbol, XElement parentElement)
+        {
+            var newElement = new XElement(XName.Get(ElementName));
+            parentElement.Add(newElement);
+            WriteElementInner(symbol, newElement);
+        }
+
+        protected abstract void WriteElementInner(MusicalSymbol symbol, XElement element);
+
+        public static MusicXmlWritingStrategyBase GetProperStrategy(Type symbolType)
+        {
+            return _strategies.FirstOrDefault(s => s.SymbolType == symbolType);
+        }
+    }
+}
