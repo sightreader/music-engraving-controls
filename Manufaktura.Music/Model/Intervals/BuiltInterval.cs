3c6181e0c07849577da74ff1bef820f1828af95f
Jacek Salamon <ajcek84@gmail.com>
2015-03-18 23:19:28 +0100

    Nowa klasa BuiltInterval

---
+++ Manufaktura.Music/Model/Intervals/BuiltInterval.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Manufaktura.Music.Model
+{
+    public class BuiltInterval : Interval
+    {
+        public Pitch StartingPitch { get; protected set; }
+
+        public Pitch EndingPitch { get; protected set; }
+
+        public BuiltInterval(Pitch pitch1, Pitch pitch2) : base(pitch2.ToStep().ToStepNumber() - pitch1.ToStep().ToStepNumber() + 1,
+            pitch2.MidiPitch - pitch1.MidiPitch)
+        {
+            StartingPitch = pitch1;
+            EndingPitch = pitch2;
+        }
+
+        public BuiltInterval(Pitch pitch1, Interval interval, Pitch.MidiPitchTranslationMode translationMode)
+            : base(interval.Steps, interval.Halftones)
+        {
+            StartingPitch = pitch1;
+            EndingPitch = pitch1.Translate(interval, translationMode);
+        }
+    }
+}
