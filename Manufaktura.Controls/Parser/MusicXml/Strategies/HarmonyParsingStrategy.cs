f7d35ea6aabb4ee4dc2edde63e8eb75f44b37408
Jacek Salamon <ajcek84@gmail.com>
2015-09-11 22:20:51 +0200

    Refaktor

--- Manufaktura.Controls/Parser/MusicXml/Strategies/HarmonyParsingStrategy.cs
+++ Manufaktura.Controls/Parser/MusicXml/Strategies/HarmonyParsingStrategy.cs
@@ -3,6 +3,7 @@ using Manufaktura.Controls.Model;
 using Manufaktura.Music.Model;
 using Manufaktura.Music.Model.Harmony;
 using Manufaktura.Music.Model.MajorAndMinor;
+using Manufaktura.Music.Xml;
 using System.Xml.Linq;
 
 namespace Manufaktura.Controls.Parser.MusicXml.Strategies
@@ -16,8 +17,8 @@ namespace Manufaktura.Controls.Parser.MusicXml.Strategies
 
         public override void ParseElement(MusicXmlParserState state, Staff staff, XElement element)
         {
-            var stepName = element.ParseChildElement("root-step");
-            staff.Elements.Add(new ChordSign(new TertianHarmony().CreateChord(new Pitch(stepName, 0, 4), 0, 2, MajorScale.C)));
+            element.IfElement("root-step").HasAnyValue().Then(s =>
+                staff.Elements.Add(new ChordSign(new TertianHarmony().CreateChord(new Pitch(s, 0, 4), 0, 2, MajorScale.C))));
         }
     }
 }
