cc98d2b107ec41eeb9e535d552df152bd65dcf71
Jacek Salamon <ajcek84@gmail.com>
2016-01-20 22:38:15 +0100

    Aplikacja testowa dla CSharpForHtml5

---
+++ Manufaktura.Controls.XamlForHtml5.Test/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Manufaktura.Controls.Model;
+using Manufaktura.Music.Model.MajorAndMinor;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Manufaktura.Controls.XamlForHtml5.Test
+{
+	public sealed partial class MainPage : Page
+	{
+		public MainPage()
+		{
+			this.InitializeComponent();
+			var score = Score.CreateOneStaffScore(Clef.BaritoneC, MajorScale.C);
+			foreach (var element in score.FirstStaff.Elements)
+			{
+
+			}
+		}
+	}
+}
