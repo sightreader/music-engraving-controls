f1baf486cd7dc253fd1f48013025f85dbda79bce
Jacek Salamon <ajcek84@gmail.com>
2016-05-14 23:30:13 +0200

    Zmiana nazwy assembly dla bibliotek legacy

--- Manufaktura.Model.Legacy/Properties/AssemblyInfo.cs
+++ Manufaktura.Model.Legacy/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Manufaktura.Model")]
+[assembly: AssemblyTitle("Manufaktura.Model.Legacy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Manufaktura.Model")]
+[assembly: AssemblyProduct("Manufaktura.Model.Legacy")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Manufaktura.Music.Legacy/Properties/AssemblyInfo.cs
+++ Manufaktura.Music.Legacy/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Manufaktura.Music")]
+[assembly: AssemblyTitle("Manufaktura.Music.Legacy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Manufaktura.Music")]
+[assembly: AssemblyProduct("Manufaktura.Music.Legacy")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
