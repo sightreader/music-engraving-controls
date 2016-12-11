e22d7db981762e92598c4fe756ef7cd0f70062cc
Jacek Salamon <ajcek84@gmail.com>
2016-12-11 13:35:12 +0100

    Test odtwarzania - Bach

--- Manufaktura.Controls.Test.Web/Controllers/HomeController.cs
+++ Manufaktura.Controls.Test.Web/Controllers/HomeController.cs
@@ -9,8 +9,9 @@ namespace Manufaktura.Controls.Test.Web.Controllers
 		public ActionResult Index()
 		{
 			var vm = new HomeViewModel();
-			
-			var serverPath = Server.MapPath("~/Content/030 Oj, zabujały 67 I nr 214.xml");
+
+			var serverPath = Server.MapPath("~/Content/JohannChristophBachFull3.0.xml");
+			//var serverPath = Server.MapPath("~/Content/030 Oj, zabujały 67 I nr 214.xml");
 			//var serverPath = Server.MapPath("~/Content/0014 Larum w obozie.xml");
 			vm.SampleScore = System.IO.File.ReadAllText(serverPath).ToScore();
 			return View(vm);
