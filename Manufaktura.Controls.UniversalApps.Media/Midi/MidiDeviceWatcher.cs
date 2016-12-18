d59e8b0a70149eb345dfe88130104f74364b2823
Jacek Salamon <ajcek84@gmail.com>
2016-12-18 23:02:42 +0100

    Midiplayer w UWP

---
+++ Manufaktura.Controls.UniversalApps.Media/Midi/MidiDeviceWatcher.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Windows.Devices.Enumeration;
+
+namespace Manufaktura.Controls.UniversalApps.Media.Midi
+{
+    internal class MidiDeviceWatcher
+    {
+        internal DeviceInformationCollection deviceInformationCollection = null;
+        internal DeviceWatcher deviceWatcher = null;
+        private bool enumerationCompleted = false;
+        private string midiSelector = string.Empty;
+
+        internal MidiDeviceWatcher(string midiSelectorString)
+        {
+            deviceWatcher = DeviceInformation.CreateWatcher(midiSelectorString);
+            midiSelector = midiSelectorString;
+
+            deviceWatcher.Added += DeviceWatcher_Added;
+            deviceWatcher.Removed += DeviceWatcher_UpdatedOrRemoved;
+            deviceWatcher.Updated += DeviceWatcher_UpdatedOrRemoved;
+            deviceWatcher.EnumerationCompleted += DeviceWatcher_EnumerationCompleted;
+        }
+
+        /// <summary>
+        /// Destructor: Remove Device Watcher events
+        /// </summary>
+        ~MidiDeviceWatcher()
+        {
+            deviceWatcher.Added -= DeviceWatcher_Added;
+            deviceWatcher.Removed -= DeviceWatcher_UpdatedOrRemoved;
+            deviceWatcher.Updated -= DeviceWatcher_UpdatedOrRemoved;
+            deviceWatcher.EnumerationCompleted -= DeviceWatcher_EnumerationCompleted;
+        }
+
+        /// <summary>
+        /// Get the DeviceInformationCollection
+        /// </summary>
+        /// <returns></returns>
+        internal DeviceInformationCollection GetDeviceInformationCollection()
+        {
+            return deviceInformationCollection;
+        }
+
+        /// <summary>
+        /// Start the Device Watcher
+        /// </summary>
+        internal void Start()
+        {
+            if (deviceWatcher.Status != DeviceWatcherStatus.Started)
+            {
+                deviceWatcher.Start();
+            }
+        }
+
+        /// <summary>
+        /// Stop the Device Watcher
+        /// </summary>
+        internal void Stop()
+        {
+            if (deviceWatcher.Status != DeviceWatcherStatus.Stopped)
+            {
+                deviceWatcher.Stop();
+            }
+        }
+
+        private async void DeviceWatcher_Added(DeviceWatcher sender, DeviceInformation args)
+        {
+            if (enumerationCompleted) deviceInformationCollection = await DeviceInformation.FindAllAsync(midiSelector);
+        }
+
+        private async void DeviceWatcher_EnumerationCompleted(DeviceWatcher sender, object args)
+        {
+            enumerationCompleted = true;
+            deviceInformationCollection = await DeviceInformation.FindAllAsync(midiSelector);
+        }
+
+        private async void DeviceWatcher_UpdatedOrRemoved(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            if (enumerationCompleted) deviceInformationCollection = await DeviceInformation.FindAllAsync(midiSelector);
+        }
+    }
+}
---
+++ Manufaktura.Controls.UniversalApps.Media/Midi/MidiTaskScorePlayer.cs
@@ -0,0 +1,53 @@
+﻿using Manufaktura.Controls.Audio;
+using Manufaktura.Controls.Model;
+using Manufaktura.Music.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Devices.Midi;
+
+namespace Manufaktura.Controls.UniversalApps.Media.Midi
+{
+    public class MidiTaskScorePlayer : TaskScorePlayer
+    {
+        private IMidiOutPort currentMidiOutputDevice;
+        private Dictionary<MidiMessageType, string> messageTypes = new Dictionary<MidiMessageType, string>();
+        private MidiDeviceWatcher midiOutDeviceWatcher;
+
+        public MidiTaskScorePlayer(Score score) : base(score)
+        {
+            midiOutDeviceWatcher = new MidiDeviceWatcher(MidiOutPort.GetDeviceSelector());
+            midiOutDeviceWatcher.Start();
+        }
+
+        public string[] GetDeviceNames()
+        {
+            return midiOutDeviceWatcher.GetDeviceInformationCollection().Select(d => d.Name).ToArray();
+        }
+
+        public async Task InitializeAsync(string deviceName)
+        {
+            var devInfo = midiOutDeviceWatcher.GetDeviceInformationCollection()?.FirstOrDefault(d => d.Name == deviceName);
+            if (devInfo == null)
+            {
+                throw new Exception("Device not found.");
+            }
+
+            currentMidiOutputDevice = await MidiOutPort.FromIdAsync(devInfo.Id).AsTask();
+        }
+
+        public override void PlayElement(MusicalSymbol element)
+        {
+            var pitchedElement = element as IHasPitch;
+            if (pitchedElement != null) return;
+
+            if (currentMidiOutputDevice == null)
+                throw new Exception($"{nameof(MidiTaskScorePlayer)} is not initialized. Run {nameof(InitializeAsync)}. " +
+                    $"To see the list of available devices use {nameof(GetDeviceNames)}.");
+
+            var midiMessageToSend = new MidiNoteOnMessage(0, (byte)pitchedElement.Pitch.MidiPitch, 127);
+            currentMidiOutputDevice.SendMessage(midiMessageToSend);
+        }
+    }
+}
