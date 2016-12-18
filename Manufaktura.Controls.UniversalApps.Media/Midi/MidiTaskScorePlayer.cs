f91b2999a91e43d3bb0254cbf45365cd4e4e625d
Jacek Salamon <ajcek84@gmail.com>
2016-12-18 23:06:54 +0100

    Midiplayer w UWP

--- Manufaktura.Controls.UniversalApps.Media/Midi/MidiTaskScorePlayer.cs
+++ Manufaktura.Controls.UniversalApps.Media/Midi/MidiTaskScorePlayer.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
 using Windows.Devices.Midi;
 
 namespace Manufaktura.Controls.UniversalApps.Media.Midi
@@ -13,26 +14,23 @@ namespace Manufaktura.Controls.UniversalApps.Media.Midi
     {
         private IMidiOutPort currentMidiOutputDevice;
         private Dictionary<MidiMessageType, string> messageTypes = new Dictionary<MidiMessageType, string>();
-        private MidiDeviceWatcher midiOutDeviceWatcher;
 
         public MidiTaskScorePlayer(Score score) : base(score)
         {
-            midiOutDeviceWatcher = new MidiDeviceWatcher(MidiOutPort.GetDeviceSelector());
-            midiOutDeviceWatcher.Start();
         }
 
-        public string[] GetDeviceNames()
+        public async Task<string[]> GetDeviceNamesAsync()
         {
-            return midiOutDeviceWatcher.GetDeviceInformationCollection().Select(d => d.Name).ToArray();
+            var devices = await DeviceInformation.FindAllAsync(MidiOutPort.GetDeviceSelector()).AsTask();
+            return devices.Select(d => d.Name).ToArray();
         }
 
         public async Task InitializeAsync(string deviceName)
         {
-            var devInfo = midiOutDeviceWatcher.GetDeviceInformationCollection()?.FirstOrDefault(d => d.Name == deviceName);
+            var devices = await DeviceInformation.FindAllAsync(MidiOutPort.GetDeviceSelector()).AsTask();
+            var devInfo = devices?.FirstOrDefault(d => d.Name == deviceName);
             if (devInfo == null)
-            {
-                throw new Exception("Device not found.");
-            }
+                throw new Exception($"Device {deviceName} not found.");
 
             currentMidiOutputDevice = await MidiOutPort.FromIdAsync(devInfo.Id).AsTask();
         }
@@ -44,7 +42,7 @@ namespace Manufaktura.Controls.UniversalApps.Media.Midi
 
             if (currentMidiOutputDevice == null)
                 throw new Exception($"{nameof(MidiTaskScorePlayer)} is not initialized. Run {nameof(InitializeAsync)}. " +
-                    $"To see the list of available devices use {nameof(GetDeviceNames)}.");
+                    $"To see the list of available devices use {nameof(GetDeviceNamesAsync)}.");
 
             var midiMessageToSend = new MidiNoteOnMessage(0, (byte)pitchedElement.Pitch.MidiPitch, 127);
             currentMidiOutputDevice.SendMessage(midiMessageToSend);
