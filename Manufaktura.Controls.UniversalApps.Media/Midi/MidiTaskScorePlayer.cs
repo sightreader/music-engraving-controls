e294cc026c7a0bccc1315dc014053eb00419ea35
jsalamo <jsalamo@APART-WSCE115>
2017-02-13 16:03:38 +0100

    Prawdopodobna przyczyna niewyłączania nut w aplikacjach UWP

--- Manufaktura.Controls.UniversalApps.Media/Midi/MidiTaskScorePlayer.cs
+++ Manufaktura.Controls.UniversalApps.Media/Midi/MidiTaskScorePlayer.cs
@@ -56,7 +56,7 @@ namespace Manufaktura.Controls.UniversalApps.Media.Midi
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             Task.Delay(note.BaseDuration.ToTimeSpan(Tempo)).ContinueWith((t,o) =>
             {
-                var midiOffMessage = new MidiNoteOffMessage(0, (byte)note.Pitch.MidiPitch, 127);
+                var midiOffMessage = new MidiNoteOffMessage((byte)actualChannelNumber, (byte)note.Pitch.MidiPitch, 127);
                 currentMidiOutputDevice.SendMessage(midiOffMessage);
                 if (pitchesPlaying[channelNumber].Contains(note.MidiPitch)) pitchesPlaying[channelNumber].Remove(note.MidiPitch);
             }, null);
