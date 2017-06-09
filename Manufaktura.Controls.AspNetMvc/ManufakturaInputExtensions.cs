54be222ec85c647ae7dd4e2d27693c22a1b2d143
Jacek Salamon <ajcek84@gmail.com>
2017-06-09 14:46:22 +0200

    Rozszerzenia Razor dla NoteViewera, akceptujące obiekt ustawień, a nie tylko wyrażenie pobierające ustawienia z modelu

--- Manufaktura.Controls.AspNetMvc/ManufakturaInputExtensions.cs
+++ Manufaktura.Controls.AspNetMvc/ManufakturaInputExtensions.cs
@@ -51,7 +51,29 @@ namespace Manufaktura.Controls.AspNetMvc
 			return NoteViewerHelper(htmlHelper, score, settings);
 		}
 
-		private static MvcHtmlString NoteViewerHelper(HtmlHelper htmlHelper, Score score, HtmlScoreRendererSettings settings)
+        [SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Justification = "This is an appropriate nesting of generic types")]
+        public static MvcHtmlString NoteViewerFor<TModel>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, string>> expression, HtmlScoreRendererSettings settings)
+        {
+            if (expression == null) throw new ArgumentNullException(nameof(expression));
+            if (settings == null) throw new ArgumentNullException(nameof(settings));
+
+            ModelMetadata metadata = ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData);
+            string musicXml = metadata.Model == null ? null : metadata.Model.ToString();
+            return NoteViewerHelper(htmlHelper, musicXml, settings);
+        }
+
+        [SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Justification = "This is an appropriate nesting of generic types")]
+        public static MvcHtmlString NoteViewerFor<TModel>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, Score>> expression, HtmlScoreRendererSettings settings)
+        {
+            if (expression == null) throw new ArgumentNullException(nameof(expression));
+            if (settings == null) throw new ArgumentNullException(nameof(settings));
+
+            ModelMetadata metadata = ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData);
+            Score score = metadata.Model == null ? null : metadata.Model as Score;
+            return NoteViewerHelper(htmlHelper, score, settings);
+        }
+
+        private static MvcHtmlString NoteViewerHelper(HtmlHelper htmlHelper, Score score, HtmlScoreRendererSettings settings)
 		{
 			IScore2HtmlBuilder builder;
 			if (settings.RenderSurface == HtmlScoreRendererSettings.HtmlRenderSurface.Canvas)
