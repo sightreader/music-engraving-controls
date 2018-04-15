c19a2ac966ba430b29fb996c6b755dd5e64a327b
Jacek Salamon <ajcek84@gmail.com>
2018-04-15 12:00:34 +0200

    Brackety w SMuFL

--- Manufaktura.Controls/Rendering/Postprocessing/AddBracketsFinishingTouch.cs
+++ Manufaktura.Controls/Rendering/Postprocessing/AddBracketsFinishingTouch.cs
@@ -62,8 +62,8 @@ namespace Manufaktura.Controls.Rendering.Postprocessing
             if (renderer.IsSMuFLFont && renderer.Settings.CurrentSMuFLMetadata != null)
             {
                 var bounds = renderer.Settings.CurrentSMuFLMetadata.GlyphBBoxes.Brace;
-                var width = bounds.BBoxNe[1] - bounds.BBoxSw[1];
-                return new Tuple<double, double>(renderer.LinespacesToPixels(bounds.BBoxNe[1] * -1), renderer.LinespacesToPixels(width));
+                var width = (bounds.BBoxNe[0] + bounds.BBoxSw[0]) * 3;  //TODO: Sprawdzić czemu Brace ma tak mały bounding box w SMuFL
+                return new Tuple<double, double>(renderer.LinespacesToPixels(bounds.BBoxSw[0] * -3 - width), renderer.LinespacesToPixels(width));
             }
             else return new Tuple<double, double>(-17.5, 15);
         }
