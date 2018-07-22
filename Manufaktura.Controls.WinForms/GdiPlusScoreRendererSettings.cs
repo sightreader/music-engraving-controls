445c6941dbb4863c2675236d30eb3f80fd664452
Jacek Salamon <ajcek84@gmail.com>
2018-07-22 18:35:31 +0200

    Minor changes

--- Manufaktura.Controls.WinForms/GdiPlusScoreRendererSettings.cs
+++ Manufaktura.Controls.WinForms/GdiPlusScoreRendererSettings.cs
@@ -1,15 +1,15 @@
 ﻿/*
  * Copyright 2018 Manufaktura Programów Jacek Salamon http://musicengravingcontrols.com/
  * MIT LICENCE
- 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
-to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
 and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
@@ -43,6 +43,8 @@ namespace Manufaktura.Controls.WinForms
                 {MusicFontStyles.TimeSignatureFont, new Font("Microsoft Sans Serif", 14.5f, FontStyle.Bold, GraphicsUnit.Pixel)}
             };
 
+        private Dictionary<MusicFontStyles, PrivateFontCollection> fontCollections = new Dictionary<MusicFontStyles, PrivateFontCollection>();
+
         public GdiPlusScoreRendererSettings()
         {
         }
@@ -65,6 +67,13 @@ namespace Manufaktura.Controls.WinForms
         public void SetFontFromPath(MusicFontStyles style, string fontPath, float fontSize, FontStyle fontStyle = FontStyle.Regular)
         {
             var privateFonts = new PrivateFontCollection();
+            if (fontCollections.ContainsKey(style))
+            {
+                fontCollections[style].Dispose();
+                fontCollections[style] = privateFonts;
+            }
+            else fontCollections.Add(style, privateFonts);
+
             privateFonts.AddFontFile(fontPath);
             SetFont(style, privateFonts.Families[0], fontSize, fontStyle);
         }
