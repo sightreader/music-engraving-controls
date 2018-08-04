63a99e45366b9845f83f2b2cf9e51f5edc62a09c
Jacek Salamon <ajcek84@gmail.com>
2018-08-04 09:49:30 +0200

    Nuget

--- Manufaktura.Controls.WPF/Properties/AssemblyInfo.cs
+++ Manufaktura.Controls.WPF/Properties/AssemblyInfo.cs
@@ -20,9 +20,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Manufaktura.Controls.WPF")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("WPF implementation of Manufaktura.Controls music notation library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Manufaktura programów Jacek Salamon")]
 [assembly: AssemblyProduct("Manufaktura.Controls.WPF")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -46,5 +46,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
