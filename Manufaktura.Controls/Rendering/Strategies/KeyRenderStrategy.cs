3bc1cc033a2a8f0f023649a8b99cdd81482bf8aa
Jacek Salamon <ajcek84@gmail.com>
2019-01-08 20:36:40 +0100

    Proper rendering of key signatures

--- Manufaktura.Controls/Rendering/Strategies/KeyRenderStrategy.cs
+++ Manufaktura.Controls/Rendering/Strategies/KeyRenderStrategy.cs
@@ -1,22 +1,24 @@
 ﻿/*
  * Copyright 2018 Manufaktura Programów Jacek Salamon http://musicengravingcontrols.com/
  * MIT LICENCE
- 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
-to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
 and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
+
 using Manufaktura.Controls.Model;
 using Manufaktura.Controls.Model.Fonts;
 using Manufaktura.Controls.Services;
 using Manufaktura.Music.Model;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Manufaktura.Controls.Rendering
@@ -26,9 +28,6 @@ namespace Manufaktura.Controls.Rendering
     /// </summary>
     public class KeyRenderStrategy : MusicalSymbolRenderStrategy<Key>
     {
-
-        public bool IsVirtualKey { get; set; }
-
         /// <summary>
         /// /// Initializes a new instance of KeyRenderStrategy
         /// </summary>
@@ -37,51 +36,82 @@ namespace Manufaktura.Controls.Rendering
         {
         }
 
+        public bool IsVirtualKey { get; set; }
         public override void Render(Key element, ScoreRendererBase renderer, FontProfile fontProfile)
         {
             if (element.Fifths != 0 && element.Measure != null && element.Measure.Elements.FirstOrDefault() == element)
                 scoreService.CursorPositionX += renderer.LinespacesToPixels(1); //Żeby był lekki margines między kreską taktową a symbolem. Być może ta linijka będzie do usunięcia
 
             scoreService.CurrentKey = element;
-            double flatOrSharpPositionY = 0;
-            bool jumpFourth = false;
-            int jumpDirection = 1;
-            int octaveShiftSharp = 0; //In G clef sharps (not flats) should be written an octave higher / W kluczu g krzyżyki (bemole nie) powinny być zapisywane o oktawę wyżej
-            if (scoreService.CurrentClef.TypeOfClef == ClefType.GClef) octaveShiftSharp = 1;
-            int octaveShiftFlat = 0;
-            if (scoreService.CurrentClef.TypeOfClef == ClefType.FClef) octaveShiftFlat = -1;
-
             if (!IsVirtualKey) element.TextBlockLocation = new Primitives.Point(scoreService.CursorPositionX, scoreService.CurrentLinePositions[0]);
 
             if (scoreService.CurrentKey.Fifths > 0)
             {
-                flatOrSharpPositionY = scoreService.CurrentClef.TextBlockLocation.Y
-                    + Pitch.StepDistance(scoreService.CurrentClef,
-                      Pitch.FromStep(Step.F, scoreService.CurrentClef.Pitch.Octave + octaveShiftSharp))
-                      * (renderer.Settings.LineSpacing / 2);
-                jumpFourth = true;
-                jumpDirection = 1;
+                var dictionary = CreateDictionary(CircleOfFifths.Sharps, CircleOfFifths.EnumerateSharps, scoreService.CurrentKey.Fifths);
+                DrawCharacters(dictionary, renderer, element, fontProfile.MusicFont.DoubleSharp, fontProfile.MusicFont.Sharp);
             }
             else if (scoreService.CurrentKey.Fifths < 0)
             {
-                flatOrSharpPositionY = scoreService.CurrentClef.TextBlockLocation.Y +
-                    Pitch.StepDistance(scoreService.CurrentClef,
-                    Pitch.FromStep(Step.B, scoreService.CurrentClef.Pitch.Octave + octaveShiftFlat))
-                    * (renderer.Settings.LineSpacing / 2);
-                jumpFourth = true;
-                jumpDirection = -1;
+                var dictionary = CreateDictionary(CircleOfFifths.Flats, CircleOfFifths.EnumerateFlats, scoreService.CurrentKey.Fifths);
+                DrawCharacters(dictionary, renderer, element, fontProfile.MusicFont.DoubleFlat, fontProfile.MusicFont.Flat);
             }
 
-            for (int i = 0; i < Math.Abs(scoreService.CurrentKey.Fifths); i++)
-            {
-                renderer.DrawCharacter(element.GetCharacter(fontProfile.MusicFont), MusicFontStyles.MusicFont, scoreService.CursorPositionX, flatOrSharpPositionY, element);
-                if (jumpFourth) flatOrSharpPositionY += 3 * 3 * jumpDirection;
-                else flatOrSharpPositionY += 3 * 4 * jumpDirection;
-                jumpFourth = !jumpFourth;
-                jumpDirection *= -1;
-                scoreService.CursorPositionX += 8;
-            }
             scoreService.CursorPositionX += 10;
+        }
+
+        private Dictionary<Step, int> CreateDictionary(Step[] collection, Func<IEnumerable<Step>> enumerator, int fifths)
+        {
+            var dictionary = collection.ToDictionary(s => s, s => 0);
+            foreach (var step in enumerator().Take(Math.Abs(scoreService.CurrentKey.Fifths)))
+            {
+                if (fifths > 0) dictionary[step]++;
+                else dictionary[step]--;
+            }
+            return dictionary;
+        }
+
+        private void DrawCharacter(char character, double line, ScoreRendererBase renderer, Key element)
+        {
+            var flatOrSharpPositionY = scoreService.CurrentClef.TextBlockLocation.Y + (scoreService.CurrentClef.Line - line) * renderer.Settings.LineSpacing;
+            renderer.DrawCharacter(character, MusicFontStyles.MusicFont, scoreService.CursorPositionX, flatOrSharpPositionY, element);
+            scoreService.CursorPositionX += 8;
+        }
+
+        private void DrawCharacters(Dictionary<Step, int> dictionary, ScoreRendererBase renderer, Key element, char doubleCharacter, char singleCharacter)
+        {
+            foreach (var stepContent in dictionary)
+            {
+                int numberOfSingleAccidentals = Math.Abs(stepContent.Value) % 2;
+                int numberOfDoubleAccidentals = Convert.ToInt32(Math.Floor((double)(Math.Abs(stepContent.Value) / 2)));
+                var line = GetHighestLineForStep(stepContent.Key);
+                for (var i = 0; i < numberOfDoubleAccidentals; i++)
+                {
+                    DrawCharacter(doubleCharacter, line, renderer, element);
+                }
+                for (var i = 0; i < numberOfSingleAccidentals; i++)
+                {
+                    DrawCharacter(singleCharacter, line, renderer, element);
+                }
+            }
+        }
+
+        private double GetHighestLineForStep(Step step)
+        {
+            var line1 = GetLine(step.ToPitch(scoreService.CurrentClef.Pitch.Octave + 1));
+
+            if (line1 < 6) return line1;
+
+            var line2 = GetLine(step.ToPitch(scoreService.CurrentClef.Pitch.Octave));
+            if (line2 < 6) return line2;
+
+            var line3 = GetLine(step.ToPitch(scoreService.CurrentClef.Pitch.Octave - 1));
+            return line3;
+        }
+
+        private double GetLine(Pitch pitch)
+        {
+            var stepDistance = (double)Pitch.StepDistance(pitch, scoreService.CurrentClef.Pitch);
+            return scoreService.CurrentClef.Line + (stepDistance / 2);
         }
     }
 }
--- Manufaktura.Music/Model/CircleOfFifths.cs
+++ Manufaktura.Music/Model/CircleOfFifths.cs
@@ -1,30 +1,42 @@
 ﻿/*
  * Copyright 2018 Manufaktura Programów Jacek Salamon http://musicengravingcontrols.com/
  * MIT LICENCE
- 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
-to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
 and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
+
 using Manufaktura.Music.Model.MajorAndMinor;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Manufaktura.Music.Model
 {
     /// <summary>
-    /// Helper class for calculations involving the circle of fifths. 
+    /// Helper class for calculations involving the circle of fifths.
     /// </summary>
     public static class CircleOfFifths
     {
+        public static Step[] Sharps => new[] { Step.F, Step.C, Step.G, Step.D, Step.A, Step.E, Step.B };
+        public static Step[] Flats => new[] { Step.B, Step.E, Step.A, Step.D, Step.G, Step.C, Step.F };
+
+        public static IEnumerable<Step> EnumerateSharps()
+        {
+            while (true) foreach (var sharp in Sharps) yield return sharp;
+        }
+
+        public static IEnumerable<Step> EnumerateFlats()
+        {
+            while (true) foreach (var flat in Flats) yield return flat;
+        }
+
         /// <summary>
         /// Calculates number of fiths for specific key described by midiPitch and flags.
         /// </summary>
@@ -95,7 +107,6 @@ namespace Manufaktura.Music.Model
                     i = -1;
                     numberOfStepsToAlter -= 6;
                 }
-
             }
             if (fifths > 0)
             {
